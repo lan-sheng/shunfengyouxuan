95a294d1e384b77d05dfcc5b83c01c5aea5778e3
李兰生 <945004642@qq.com>
2019-12-16 22:39:17 +0800

    更新文件

---
+++ src/script/index/animation.js
@@ -0,0 +1,48 @@
+function getstyle(el, property) {
+    if (getComputedStyle) {
+        return getComputedStyle(el)[property]
+    } else {
+        return el.currentstyle[property];
+    }
+}
+
+// function animate(el, property, target) { //el:元素;property:属性;target:目标;
+//     clearInterval(el.timerId)
+//     el.timerId = setInterval(function() {
+//         var current;
+//         if (property === 'opacity') {
+//             current = Math.round(parseFloat(getstyle(el, 'opacity')) * 100)
+//         } else {
+//             current = parseInt(getstyle(el, property))
+//         }
+//         var speed = (target - current) / 30;
+//         speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+//         if (property === 'opacity') {
+//             el.style.opacity = ((current + speed) / 100)
+//         } else {
+//             el.style[property] = (current + speed) + 'px';
+//         }
+//     }, 10)
+// }
+
+function animate(el, properties) { //el:元素;properties{property:属性;target:目标};
+    clearInterval(el.timerId)
+    el.timerId = setInterval(function() {
+        for (var property in properties) {
+            var current;
+            var target = properties[property];
+            if (property === 'opacity') {
+                current = Math.round(parseFloat(getstyle(el, 'opacity')) * 100)
+            } else {
+                current = parseInt(getstyle(el, property))
+            }
+            var speed = (target - current) / 30;
+            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+            if (property === 'opacity') {
+                el.style.opacity = ((current + speed) / 100)
+            } else {
+                el.style[property] = (current + speed) + 'px';
+            }
+        }
+    }, 10)
+}
